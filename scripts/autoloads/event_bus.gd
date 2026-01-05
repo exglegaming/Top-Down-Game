2812f34a600e05f320ecdc10130ac04dc14bfc1e
ExgleGaming <EaglesFan91x@outlook.com>
2026-01-05 00:16:07 -0500

    removed all gdscript files

--- scripts/autoloads/event_bus.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-
-
-@warning_ignore("unused_signal")
-signal on_player_health_updated(current_health: float, max_health: float)
--- scripts/resources/data/player/player_data.gd
+++
@@ -1,9 +0,0 @@
-class_name PlayerData
-extends Resource
-
-
-@export var icon: Texture2D
-@export var id: String
-@export var max_hp: float = 5.0
-@export var move_speed: float = 200.0
-@export var magic: float = 1.0
